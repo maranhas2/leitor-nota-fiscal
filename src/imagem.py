072ad66c53984d50599c0627b59f5b9048310df1
maranhas02 <rodrigo.maranhao2@gmail.com>
2025-11-19 16:22:44 -0300

    Teste de leitura com pytesseract

--- src/imagem.py
+++ src/imagem.py
@@ -1,31 +1,25 @@
 import cv2
+import pytesseract
 
-# Carregando a imagem
-imagem_path = 'imagens/NFSe_ficticia.jpg'  # Substitua pelo caminho da sua imagem
-imagem = cv2.imread(imagem_path)
-
-# Verificando se a imagem foi carregada corretamente
-if imagem is None:
-    print("Erro ao carregar a imagem.")
-else:
-    print("Imagem carregada com sucesso.")
+# Carregando a imagem do documento
+imagem_documento_path = 'imagens/NFSe_ficticia.jpg'
+imagem_documento = cv2.imread(imagem_documento_path)
 
 # Convertendo a imagem para escala de cinza
-imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+imagem_documento_cinza = cv2.cvtColor(imagem_documento, cv2.COLOR_BGR2GRAY)
 
 # Aplicando binarização (thresholding)
-_, imagem_binaria = cv2.threshold(imagem_cinza, 128, 255, cv2.THRESH_BINARY)
+_, imagem_documento_binaria = cv2.threshold(imagem_documento_cinza, 128, 255, cv2.THRESH_BINARY)
 
-# # Aplicando um filtro para remover ruídos (opcional)
-# imagem_sem_ruido = cv2.medianBlur(imagem_binaria, 1)
+# Aplicando um filtro para remover ruídos (opcional)
+imagem_sem_ruido = cv2.medianBlur(imagem_documento_binaria, 1)
 
 # Ajustando o contraste (opcional)
 alpha = 1.5  # Fator de contraste
 beta = 50    # Fator de brilho
-imagem_processada = cv2.convertScaleAbs(imagem_binaria, alpha=alpha, beta=beta)
+imagem_processada = cv2.convertScaleAbs(imagem_sem_ruido, alpha=alpha, beta=beta)
 
-# Exibindo a imagem original e a imagem processada
-cv2.imshow('Imagem Original', imagem)
-cv2.imshow('Imagem Processada', imagem_processada)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Aplicando OCR na imagem do documento
+texto_documento = pytesseract.image_to_string(imagem_processada, lang='por')
+
+print(f'Texto no Documento: {texto_documento}')
