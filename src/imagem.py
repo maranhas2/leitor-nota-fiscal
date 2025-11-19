678aba8382c78fca119e00500a460f12fd58b587
maranhas02 <rodrigo.maranhao2@gmail.com>
2025-11-19 16:27:45 -0300

    Adiciona conversão de PDF para foto e verificação se o arquivo é uma foto ou um PDF

--- src/imagem.py
+++ src/imagem.py
@@ -1,12 +1,35 @@
 import cv2
 import pytesseract
 
-# Carregando a imagem do documento
-imagem_documento_path = 'imagens/NFSe_ficticia.jpg'
-imagem_documento = cv2.imread(imagem_documento_path)
+import pytesseract
+import cv2
+import re
+
+from pdf2image import convert_from_path
+
+caminho = 'imagens/NFSe_ficticia_layout_completo.pdf'
+
+if caminho[-4:] == ".pdf":
+    pages = convert_from_path(caminho)
+    if len(pages) > 1:
+        caminhos = []
+        for i in range(len(pages)):
+            pages[i].save(f"{caminho[:-4]}{str(i)}.jpg", 'JPEG')
+            caminhos.append(f"{caminho[:-4]}{str(i)}.jpg")
+    else:
+        pages[0].save(f"{caminho[:-4]}.jpg", 'JPEG')
+        caminhos = f"{caminho[:-4]}.jpg"
+elif caminho[-4:] ==".jpg" or caminho[-4:] ==".png":
+    caminhos = caminho
+
+# Carregando a imagem
+imagem_path = caminhos  # Substitua pelo caminho da sua imagem
+imagem = cv2.imread(imagem_path)
+
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'  
 
 # Convertendo a imagem para escala de cinza
-imagem_documento_cinza = cv2.cvtColor(imagem_documento, cv2.COLOR_BGR2GRAY)
+imagem_documento_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 # Aplicando binarização (thresholding)
 _, imagem_documento_binaria = cv2.threshold(imagem_documento_cinza, 128, 255, cv2.THRESH_BINARY)
